0594b2cd6a53c2549809be5681b65c5ac9766a79
yuvischwartz <yuval.orderdesk@gmail.com>
2022-04-27 06:59:34 -0200

    added the answers

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import CategoriesList from './components/CategoriesList';
 function App() {
   
   const [selectedCategory, setSelectedCategory] = useState('categoryOne')
+  const [activeQuestion, setActiveQuestion] = useState('')
   const [questionTxt, setQuestionTxt] = useState('')
   const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
+  const [answerTxt, setAnswerTxt] = useState('')
   const [click, setClick] = useState(false);
   const [categoriesListClick, setCategoriesListlick] = useState(false);
+  // const [answerClicked,setAnswerClicked] = useState(false);
   
   const handleClick = () => {
     setClick(!click);
@@ -21,17 +25,16 @@ function App() {
     setCategoriesListlick(!categoriesListClick);
     console.log('clicked')
   }
-
-    const fetchQuestions = async () => {
-        let res = await fetch(`http://localhost:3000/api/categories?category=${selectedCategory}/questions`)
+  const fetchQuestions = async () => {
+        let res = await fetch(`http://localhost:3000/api/categories?categoryId=${selectedCategory.id}/questions`)
         let data = await res.json()
-        console.log(data)
+        console.log('data',data)
         setQuestions(data)
     };
-
-    useEffect(() => {
-        fetchQuestions()
-    }, [])
+    
+    // useEffect(() => {
+    //     fetchQuestions()
+    // }, [])
 
     useEffect(() => {
         fetchQuestions()
@@ -42,40 +45,98 @@ function App() {
     };
 
     const createQuestion = async () => {
-        let res = await fetch(`http://localhost:3000/api/categories?category=${selectedCategory}/questions`, {
+        let res = await fetch(`http://localhost:3000/api/categories?categoryId=${selectedCategory.id}/questions`, {
             method: 'POST',
             body: JSON.stringify({questionTxt: questionTxt}),
             headers: {
                 'Content-Type': 'application/json'
             },
         })
-        let data = await res.json()
-        console.log(data)
-        setQuestionTxt('')
-        fetchQuestions()
+      let data = await res.json()
+      console.log('created');
+      console.log(data)
+      setQuestionTxt('')
+      fetchQuestions()
     };
 
+    
+      // Answers
+      const fetchAnswers = async () => {
+        let res = await fetch(`http://localhost:3000/api/questions/?questionId=${activeQuestion.id}/answers`)
+        let data = await res.json()
+        console.log('data',data)
+        setQuestions(data)
+    };
+    const updateAnswerTxt = async (event) => {
+      setAnswerTxt(event.currentTarget.value)
+    };
+    const createAnswer = async () => {
+      let res = await fetch(`http://localhost:3000/api/questions/?questionId=${activeQuestion.id}/answers`, {
+          method: 'POST',
+          body: JSON.stringify({answerTxt: answerTxt}),
+          headers: {
+              'Content-Type': 'application/json'
+          },
+      })
+      let data = await res.json()
+      console.log('created');
+      console.log(data)
+      setAnswerTxt('')
+      fetchAnswers()
+    };
   return (
     <>
       <Navbar handleClick={handleClick} click={click} categoriesListClick={categoriesListClick} setCategoriesListlick={setCategoriesListlick} categoriesClick={categoriesClick}/>
       {click && <DropDown handleClick={handleClick} />}
       
-      {categoriesListClick && <CategoriesList selectedCategory={selectedCategory} selectedCategory={selectedCategory} />}
+      {categoriesListClick && <CategoriesList selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />}
       
-      <div className={'h-screen w-screen bg-gray-50'}>
+      
+      <h1>{selectedCategory.id}</h1>
+
+
+      <div className={'w-screen h-screen bg-violet-100'}>
+      <div className={'w-1/2 h-screen bg-gray-100'}>
+          <div className={'flex justify-center'}>
+            <h1 className={'logo2 text-xl mt-10'}>{selectedCategory}</h1>
+            <div className={'flex justify-center p-12 gap-12'}>
+                    <textarea className={'border rounded'} value={questionTxt} onChange={updateQuestionTxt}  id="" cols="30" rows="5"></textarea>
+                    <button onClick={createQuestion} className={'px-2 py-1 text-white rounded h-8 bg-violet-600'}>Create Question</button>
+                </div>
+          </div>
+          <h2 className={'logo2 text-xl mt-10'}>Question List for {selectedCategory}</h2>
+          {questions && <ul>
+          {questions.map((question) => {
+            return <li onClick={() => setActiveQuestion(question.id)} key={question.id}>{question.questionTxt}</li>
+          })}
+        </ul>}
+      </div>
+      <div className={'w-1/2 h-screen bg-violet-200'}>
+          {activeQuestion &&
+            <div>
+            <h1>Answers for: {activeQuestion}</h1>
+            <div className={'flex justify-center p-12 gap-12'}>
+                    <textarea className={'border rounded'} value={answerTxt} onChange={updateAnswerTxt}  id="" cols="30" rows="3"></textarea>
+                    <button onClick={createAnswer} className={'px-2 py-1 text-white rounded h-8 bg-violet-600'}>Create Answer</button>
+            </div>
+            </div>}
+      </div>
+      </div>
+      {/* <div className={'h-screen w-screen bg-gray-50'}>
         <h1 className={'text-rigth text-xl font-ligth text-black mx-10 pt-5'}>Category {selectedCategory}</h1> 
         <div className={'flex justify-start p-12 gap-12'}>
-          <textarea className={'items-center border'}value={questionTxt} onChange={updateQuestionTxt}  id="" cols="30" rows="5"></textarea>
-          <button onClick={createQuestion} className={'px-2 py-1 text-white rounded h-8 bg-blue-500'}>Create Question</button>
+        <textarea className={'items-center border'}value={questionTxt} onChange={updateQuestionTxt}  id="" cols="30" rows="5"></textarea>
+        <button onClick={createQuestion} className={'px-2 py-1 text-white rounded h-8 bg-blue-500'}>Create Question</button>
       </div>
 
+        <h1>Questions List for {selectedCategory}</h1>
         {questions && <ul>
           {questions.map((question) => {
             return <li key={question.id}>{question.questionTxt}</li>
           })}
         </ul>}
 
-      </div>
+      </div> */}
 
       {/* <Routes>
           <Route path="/" element={<Home/>} /> 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,13 @@ import React from 'react'
 function Navbar(props) {
   return (
     <div>
-      <ul className={'p-4 bg-blue-600 text-white flex flex-row sm:justify-start justify-between relative'}>
-          <li className={'logo2 text-center text-xl uppercase tracking-wide pl-5 font-semibold cursor-pointer'}>Question App</li>
-          <li className={'hidden sm:inline-block font text-sm ml-20 pt-1 cursor-pointer uppercase'}>About Us</li>
-          <li className={'hidden sm:inline-block font text-sm ml-10 pt-1 cursor-pointer uppercase'}>Contact Us</li>
-          <li className={'hidden sm:inline-block font text-sm ml-10 pt-1 cursor-pointer uppercase'}>Security</li>
-          <li onClick={props.handleClick} className={'sm:hidden text-xl cursor-pointer'}><i  className={props.click ? 'fa-solid fa-xmark text-white' : 'fa-solid fa-bars text-white'}></i></li>
+      <ul className={'p-4 bg-violet-500 text-white flex flex-row sm:justify-start justify-between relative '}>
+          <li className={'font1 text-center text-xl uppercase tracking-wide pl-5 font-semibold cursor-pointer'}>Question App</li>
+          <li className={'font1  hidden sm:inline-block font text-sm ml-20 pt-1 cursor-pointer uppercase'}>About Us</li>
+          <li className={'font1  hidden sm:inline-block font text-sm ml-10 pt-1 cursor-pointer uppercase'}>Contact Us</li>
+          <li className={'font1  hidden sm:inline-block font text-sm ml-10 pt-1 cursor-pointer uppercase'}>Security</li>
+        <li onClick={props.handleClick} className={'sm:hidden text-xl cursor-pointer'}><i className={props.click ? 'fa-solid fa-xmark text-white' : 'fa-solid fa-bars text-white'}></i></li>
+        <li className={'absolute right-20 pt-1'}><button className={'px-4 py-1 hidden sm:inline-block text-white text-sm bg-black rounded-full'}>Log In</button></li>
         <li onClick={props.categoriesClick} className={'absolute right-8 '}><button onClick={props.categoriesClick}><i onClick={props.categoriesClick} className={' px-2 float-right hidden sm:inline-block fa-solid fa-caret-down text-white text-2xl'}/></button></li>
       </ul>
       
